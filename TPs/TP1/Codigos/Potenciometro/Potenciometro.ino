d56eb32ce8b167ac76b4f881332acf516c972a5a
smabrera03 <santimcabrera@gmail.com>
2026-04-11 14:51:17 -0300

    lol

--- TPs/TP1/Codigos/Potenciometro/Potenciometro.ino
+++ TPs/TP1/Codigos/Potenciometro/Potenciometro.ino
@@ -1,9 +1,19 @@
-void setup() {
-  // put your setup code here, to run once:
+// El potenciómetro tiene algo de ruido (a veces glitchea)
 
+void setup() {
+  Serial.begin(115200); // Inicia la comunicación serial
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 
+  // Punto 1
+  unsigned long t_ini = micros();
+
+  int valor = analogRead(A0);
+
+  // Punto 2
+  int angulo = map(valor, 0, 1023, 0, 270); 
+  Serial.println(angulo);
+
+  while (micros() - t_ini < 20000) {}
 }
