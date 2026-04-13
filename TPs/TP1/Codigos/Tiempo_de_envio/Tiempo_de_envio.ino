b867f0932152b17f5ac9c765b594f3069410a91c
smabrera03 <santimcabrera@gmail.com>
2026-04-13 08:52:45 -0300

    Cambié un poco el código para medir tiempo

--- TPs/TP1/Codigos/Tiempo_de_envio/Tiempo_de_envio.ino
+++ TPs/TP1/Codigos/Tiempo_de_envio/Tiempo_de_envio.ino
@@ -1,29 +1,32 @@
+#define N 100
+
 void setup() {
   Serial.begin(115200);
 
 }
 
 void loop() {
-  unsigned long t_ini = micros();
   float dato = 4.5;
+  Serial.flush(); //Vacía el buffer
 
-  for(int i = 0; i < 100; i++){
+  unsigned long t_ini = micros();
+
+  for(int i = 0; i < N; i++){
     matlab_send(&dato, 1);
-    dato = dato + 1;
   }
 
+  Serial.flush(); //Espera que el buffer se vacíe. 
   unsigned long t_fin = micros();
   
-  Serial.println('El envío tardó: ');
-  Serial.println((t_fin - t_ini)/100);
+  Serial.println("");
+  Serial.print("En envio tardo: ");
+  Serial.println((float)(t_fin - t_ini)/N);
 
   while(micros() - t_ini < 500000){}
 }
 
 
 void matlab_send(float *datos, size_t largo){
-  Serial.write("abcd");
-
   for(size_t i = 0; i < largo; i++){
     byte * b = (byte *) &datos[i];
     Serial.write(b,4);
@@ -31,6 +34,5 @@ void matlab_send(float *datos, size_t largo){
 }
 
 /*
-Si envío siempre el mismo float, tardo 625 us por float
-El envío tarda 625 us por float
+El envío tarda 340,12 us por float
 */
